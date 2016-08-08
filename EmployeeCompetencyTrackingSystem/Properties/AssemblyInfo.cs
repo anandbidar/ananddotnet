5400621c29e05627cfe0d0a3da369814b7b9d051
RojaP <9999pranav@gmail.com>
2016-08-08 10:57:02 +0530

    added attributes to supress fxcop messages related to strong name assembly and cls compliant

--- EmployeeCompetencyTrackingSystem/Properties/AssemblyInfo.cs
+++ EmployeeCompetencyTrackingSystem/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EmployeeCompetencyTrackingSystem")]
+[assembly: AssemblyTitle("EmployeeCompetencyTrackingSystem.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hewlett-Packard Company")]
-[assembly: AssemblyProduct("EmployeeCompetencyTrackingSystem")]
+[assembly: AssemblyProduct("EmployeeCompetencyTrackingSystem.Tests")]
 [assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("75d643b7-e6a4-4506-94dc-6863436cd97b")]
+[assembly: Guid("8ebe40e6-7790-4cfa-8451-296757ed72b0")]
 
 // Version information for an assembly consists of the following four values:
 //
